bc72c7d321e1d258a832ad481b9fe3807f334625
Miguel Costa <Miguel.Fernandes.Costa@formandos.cinel.pt>
2020-09-29 22:30:46 +0100

    Add GenericRepositpry

---
+++ ShopCet47.Web/Data/Entities/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCet47.Web.Data.Entities
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+
+        //bool wasDeleted { get; set; }
+
+    }
+}
